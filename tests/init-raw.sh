9caec87822d7f5d9b463197a6ef49b95d4197471
Alvaro del Castillo <acs@bitergia.com>
2018-03-08 17:42:42 +0100

    [tests] Use askbot archive and use for raw index the pattern test_<ds>-raw

--- tests/init-raw.sh
+++ tests/init-raw.sh
@@ -2,10 +2,9 @@ CACHE=perceval-cache
 RAW2FILE=./raw2file.py
 ES="http://bitergia:bitergia@172.17.0.1:9200"
 echo Using $ES as the Elasticsearch endpoint. Adjust it to your endpoint.
-$RAW2FILE -g  -p -e $ES -i functest_test-raw -f $CACHE/raw/functest.json
-$RAW2FILE -g  -p -e $ES -i google_hits_test-raw -f $CACHE/raw/google_hits.json
-$RAW2FILE -g  -p -e $ES -i apache_test-raw -f $CACHE/raw/apache.json
-$RAW2FILE -g  -p -e $ES -i twitter_test-raw -f $CACHE/raw/twitter.json
-$RAW2FILE -g  -p -e $ES -i askbot_test-raw -f $CACHE/raw/askbot.json
-$RAW2FILE -g  -p -e $ES -i remo_activities_test-raw -f $CACHE/raw/remo_activities.json
-$RAW2FILE -g  -p -e $ES -i crates_test-raw -f $CACHE/raw/crates.json
+$RAW2FILE -g  -p -e $ES -i test_functest-raw -f $CACHE/raw/functest.json
+$RAW2FILE -g  -p -e $ES -i test_google_hits-raw -f $CACHE/raw/google_hits.json
+$RAW2FILE -g  -p -e $ES -i test_apache-raw -f $CACHE/raw/apache.json
+$RAW2FILE -g  -p -e $ES -i test_twitter-raw -f $CACHE/raw/twitter.json
+$RAW2FILE -g  -p -e $ES -i test_remo_activities-raw -f $CACHE/raw/remo_activities.json
+$RAW2FILE -g  -p -e $ES -i test_crates-raw -f $CACHE/raw/crates.json
